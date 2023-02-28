d035eed34d6695cef185c844c347992f2b4ef7ad
ChristianCalvin02 <christian.calvinwijaya02@gmail.com>
2023-02-28 17:33:51 +0700

    Tambah activity change password

---
+++ app/src/main/java/com/christian/native160420034/ChangePasswordActivity.kt
@@ -0,0 +1,13 @@
+package com.christian.native160420034
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+
+        //change password
+    }
+}
